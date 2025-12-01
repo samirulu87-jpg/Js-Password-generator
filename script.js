3da568f700be32f6c0969462b5f6cce7f0039fd4
Samantha Anderson <samirulu87@gmail.com>
2025-12-01 11:49:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+//Define varables 
+const genbtn= document.getElementById("generate")
+const lengthInput = document.getElementById("length"); 
+
+
+
+
+genbtn.addEventListener("click", () => {
+  const lengthValue = Number(lengthInput.value);
+
+
+let result = generateRandomLetters(lengthValue);
+
+
+    if (document.querySelector('#Uppercase').checked) {
+    result = applyUppercase(result);
+  }
+  
+  if (document.querySelector('#numbers').checked) {
+     result = applyNumbers(result);
+  }
+  
+  if (document.querySelector('#symbols').checked) {
+    result = applySymbols(result);
+  }
+
+ document.getElementById("password").value = result;
+
+}) 
+
+//Uppercase replace function
+function applyUppercase(str) {
+  const chars = str.split(""); 
+  for (let i = 0; i < chars.length; i++) {
+    if (Math.random() < 0.5) {
+      chars[i] = chars[i].toUpperCase();
+    }
+  }
+  return chars.join(""); 
+};
+
+//Number replace function
+function applyNumbers(str) {
+  const chars = str.split("");
+  for (let i = 0; i < chars.length; i++) {
+    if (Math.random() < 0.3) { 
+      chars[i] = Math.floor(Math.random() * 10).toString();
+    }
+  }
+  return chars.join("");
+}
+
+//Letter length generator 
+ function generateRandomLetters(length) {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * letters.length);
+    result += letters[randomIndex];
+  }
+ 
+   return result;
+};
+
+//Symbol replace generator 
+function applySymbols(str) {
+  const chars = str.split("");
+  const symbols = "!@#$%^&*()_+-={}[]<>?/|~";
+
+  for (let i = 0; i < chars.length; i++) {
+    if (Math.random() < 0.3) {  
+      const r = Math.floor(Math.random() * symbols.length);
+      chars[i] = symbols[r];
+    }
+  }
+
+  return chars.join("");
+};
+
+
+
+
+
+
+
+//random letters
+//random numbers 
+//randomly uparcase the letters
+//radom symbols 
+//adds varables if the check box is checked 
+//function that disalys the string (updates each time the button is clicked) 
+
